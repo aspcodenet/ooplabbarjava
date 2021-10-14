ce78cf2582a0b817c14150be555128faabf653db
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-14 15:34:20 +0200

    4

---
+++ src/main/java/se/systementor/Lab4.java
@@ -0,0 +1,10 @@
+package se.systementor;
+
+public class Lab4 {
+    public void run(){
+        Person p = new Person("Stefan",49);
+
+        boolean b = p.isAdult();
+        String greet = p.greetAsRovarSprak(); 
+    }
+}
